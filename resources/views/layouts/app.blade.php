c59fda48ff17c546f374dbc24550bbd2fcd1db1c
Mohammad Anzer <U1969730@unimail.hud.ac.uk>
2025-05-02 12:55:27 +0100

    Fix: Correct Vite asset inclusion

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -13,19 +13,9 @@
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
     <link href="https://fonts.bunny.net/css?family=Nunito" rel="stylesheet">
 
-
-    <link href="{{asset('css/app.css')}}" rel="stylesheet" type="text/css">
-    <script src="{{asset('js/app.js')}}"></script>
-
-   
-    <!-- Scripts -->
+    <!-- ✅ Load assets using Vite -->
     @vite(['resources/sass/app.scss', 'resources/js/app.js'])
-
-
-
 </head>
-
-
 
 
 
